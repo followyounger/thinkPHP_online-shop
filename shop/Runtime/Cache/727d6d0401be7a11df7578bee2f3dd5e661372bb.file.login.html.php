2341bf56110420077ffa5a9e127b575d31e4d756
followyounger <zywangzhaoyang@163.com>
2017-07-28 19:55:04 +0800

    修改了权限管理
    之前的权限管理，存在问题

--- shop/Runtime/Cache/727d6d0401be7a11df7578bee2f3dd5e661372bb.file.login.html.php
+++ shop/Runtime/Cache/727d6d0401be7a11df7578bee2f3dd5e661372bb.file.login.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.6, created on 2017-07-19 17:02:51
+<?php /* Smarty version Smarty-3.1.6, created on 2017-07-28 14:26:38
          compiled from "D:/phpStudy/WWW/tp3/shop/Admin/View\Manager\login.html" */ ?>
 <?php /*%%SmartyHeaderCode:29581596ec88b28cda0-21867945%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '727d6d0401be7a11df7578bee2f3dd5e661372bb' => 
     array (
       0 => 'D:/phpStudy/WWW/tp3/shop/Admin/View\\Manager\\login.html',
-      1 => 1500454687,
+      1 => 1501134841,
       2 => 'file',
     ),
   ),
@@ -46,13 +46,13 @@ User_Login.css" type="text/css" rel="stylesheet" />
                             <li class="user_main_c">
                                 <div class="user_main_box">
                                     <ul>
-                                        <li class="user_main_text">用户名： </li>
-                                        <li class="user_main_input">
-                                            <input class="TxtUserNameCssClass" id="admin_user" maxlength="20" name="admin_user"> </li></ul>
-                                    <ul>
-                                        <li class="user_main_text">密&nbsp;&nbsp;&nbsp;&nbsp;码： </li>
-                                        <li class="user_main_input">
-                                            <input class="TxtPasswordCssClass" id="admin_psd" name="admin_psd" type="password">
+    <li class="user_main_text">用户名： </li>
+      <li class="user_main_input">
+      <input class="TxtUserNameCssClass" id="admin_user" maxlength="20" name="admin_user"> </li></ul>
+<ul>
+     <li class="user_main_text">密&nbsp;&nbsp;&nbsp;&nbsp;码： </li>
+      <li class="user_main_input">
+      <input class="TxtPasswordCssClass" id="admin_psd" name="admin_psd" type="password">
                                         </li>
                                     </ul>
  <ul>
@@ -64,11 +64,11 @@ User_Login.css" type="text/css" rel="stylesheet" />
 /verifyImg/'+Math.random()" alt="" />
     </li>
    </ul>
-                            </div>
-                            </li>
-                            <li class="user_main_r">
+     </div>
+     </li>
+      <li class="user_main_r">
 
-                                <input style="border: medium none; background: url('<?php echo @ADMIN_IMG_URL;?>
+      <input style="border: medium none; background: url('<?php echo @ADMIN_IMG_URL;?>
 user_botton.gif') repeat-x scroll left top transparent; height: 122px; width: 111px; display: block; cursor: pointer;" value="" type="submit">
                             </li>
                         </ul>
--- shop/Tools/AdminController.class.php
+++ shop/Tools/AdminController.class.php
@@ -1,18 +1,18 @@
 <?php
 //后台普通控制器的父类
-namespace Tools;
-use Think\Controller;
+// namespace Tools;
+// use Think\Controller;
 
-class AdminController extends Controller{
-    function __construct(){
-        parent::__construct();
-            //用户访问权限控制
-            //获得当前正在访问的“控制器-操作方法”
-        $nowac = CONTROLLER_NAME.'-'.ACTION_NAME;
-        //获得当前用户对应角色的权限列表信息
+// class AdminController extends Controller{
+//     function __construct(){
+//         parent::__construct();
+//             //用户访问权限控制
+//             //获得当前正在访问的“控制器-操作方法”
+//         $nowac = CONTROLLER_NAME.'-'.ACTION_NAME;
+//         //获得当前用户对应角色的权限列表信息
 
-        $admin_name = session('admin_name');
-        //$rang_ac = "Manger-login,Manager-verifyImg";
+//         $admin_name = session('admin_name');
+//         $rang_ac = "Manger-login,Manager-verifyImg";
 
 //        if(empty($admin_name) && strpos($rang_ac,$nowac)===false){
 //             //$this -> redirect('Manager/login');
@@ -24,25 +24,80 @@ class AdminController extends Controller{
 //             echo $js;
 //         }
 
-        $admin_id = session('admin_id');
-        $admin_info = D('Manager')->find($admin_id);
-        $role_id = $admin_info['mg_role_id'];
-        $role_info = D('Role')->find($role_id);
-        $role_auth_ac = $role_info['role_auth_ac'];
+//         $admin_id = session('admin_id');
+//         $admin_info = D('Manager')->find($admin_id);
+//         $role_id = $admin_info['mg_role_id'];
+//         $role_info = D('Role')->find($role_id);
+//         $role_auth_ac = $role_info['role_auth_ac'];
 
-        $allowac = "Manager-login,Manager-logout,Manager-verifyImg,Index-left,Index-right,Index-head,Index-index";
+//         $allowac = "Manager-login,Manager-logout,Manager-verifyImg,Index-left,Index-right,Index-head,Index-index";
 
-        //在一个大的字符串中判断一个小的字符串从左边开始第一次出现的位置
-        //dump(strpos($nowac,$role_auth_ac));
-        if (strpos($role_auth_ac,$nowac)===false &&strpos($allowac,$nowac)===false && admin_name!=='admin') {
-            # code...
-            exit('没有访问权限');
+//         //在一个大的字符串中判断一个小的字符串从左边开始第一次出现的位置
+//         //dump(strpos($nowac,$role_auth_ac));
+//         if (strpos($role_auth_ac,$nowac)===false &&strpos($allowac,$nowac)===false && admin_name!=='admin') {
+//             # code...
+//             exit('没有访问权限');
+//         }
+
+
+//     }
+// }
+
+namespace Tools;
+use Think\Controller;
+
+class AdminController extends Controller{
+    //构造方法
+    function __construct(){
+        //为了避免覆盖父类的构造方法，先执行父类的构造方法
+        parent::__construct();
+        //用户访问权限控制
+        //获得当前正在访问的"控制器-操作方法 nowac"
+        //判断nowac是否在用户的角色权限列表里边存在
+        //① 当前请求的控制器-操作方法
+        $nowac = CONTROLLER_NAME."-".ACTION_NAME;
+
+        $admin_name = session('admin_name');
+        //用户没有登录系统，就使其退出并进入到登录页面
+        //有一些操作，允许在"退出的状态"也让访问
+        $rang_ac = "Manager-login,Manager-verifyImg";
+        //① 用户不在登录状态
+        //② 用户的操作 还没有在$rang_ac出现
+        if(empty($admin_name) && strpos($rang_ac,$nowac)===false){
+            //$this -> redirect('Manager/login');
+            $js = <<<eof
+                <script type="text/javascript">
+                window.top.location.href="/shop/index.php/Admin/Manager/login";
+                </script>
+eof;
+            echo $js;
         }
 
+        //② 获得当前用户对应角色的权限列表信息
+        //用户----组别----权限
+        //用户信息
+        $admin_id = session('admin_id');
+        $admin_info = D('Manager')->find($admin_id);
+        //角色信息
+        $role_id = $admin_info['mg_role_id'];
+        $role_info = D('Role')->find($role_id);
+        //拥有的权限信息
+        $auth_ac = $role_info['role_auth_ac'];
 
+        //设置默认允许访问的权限
+        $allowac = "Manager-login,Manager-logout,Manager-verifyImg,Index-left,Index-right,Index-head,Index-index";
+
+        //判断：$nowac是否在$auth_ac存在
+        //strpos(大字符串，小字符串) 在一个大的字符串中判断一个小的字符串从左边开始第一次出现的位置
+        //         "hello beijing tianjin beijing"   "beijing"
+        //① 当前访问的权限没有在"角色对应的权限列表"
+        //② 当前访问的权限也没有在"默认允许列表里边"
+        //③ 当前用户还不能是超级管理员admin
+        if(strpos($auth_ac,$nowac)===false && strpos($allowac,$nowac)===false && $admin_name!=='admin'){
+            exit('没有访问权限！');
+        }
     }
 }
-
 
 
  ?>
